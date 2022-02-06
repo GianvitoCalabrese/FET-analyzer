900e0731f38517ee684d2db8e0deed922f830671
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-02-06 19:32:40 +0100

    Update GFET analyzer
    updates in order to more use the cust_mod...

--- GFETAnalysis.py
+++ GFETAnalysis.py
@@ -32,7 +32,7 @@ for child in root:
     ch_l = int(child[0].text)
     print(name)
     img = cv2.rectangle(img, ((int(name[1]))*80-80, (n_y - int(name[0]))*80),
-                        ((int(name[1]))*80, (n_y - int(name[0]))*80-80), cust_mod.col_scheme(ch_l), -1)
+                        ((int(name[1]))*80, (n_y - int(name[0]))*80-80), cust_mod.db_conn(ch_l).db_conn(loc_host="127.0.0.1" , user = "root" , db ="tutorial_sql", db_tab="gate_polimi"), -1)
     img = cv2.putText(img, name, ((
         int(name[1]))*80 - 80, (n_y - int(name[0]))*80-10), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.imwrite('map.png', img)
