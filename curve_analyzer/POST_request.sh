173f76da5842566db388726e972d55c5aedebd95
Gianvito Calabrese <gcalabrese296@gmail.com>
2024-07-20 15:30:58 +0200

    small adjustment

---
+++ curve_analyzer/POST_request.sh
@@ -0,0 +1,1 @@
+curl -X POST -d "param1=value1&param2=value2" http://127.0.0.1:8000/analyze_data/
--- project.sh
+++ project.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+# Attiva l'ambiente Anaconda
+source C:/Users/tele1/anaconda3/etc/profile.d/conda.sh
+conda activate python3115
 
 cd curve_analyzer
 python manage.py makemigrations
