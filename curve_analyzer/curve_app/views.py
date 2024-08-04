f009523a051f86c880bec252768d79f36abbd510
Gianvito Calabrese <gcalabrese296@gmail.com>
2024-08-04 21:18:35 +0200

    some useful modification...

--- curve_analyzer/curve_app/views.py
+++ curve_analyzer/curve_app/views.py
@@ -21,36 +21,36 @@ def analyze_data(request):
         print(f'Colonna y selezionata: {y_column}')  # Debugging
 
         lines = file.readlines()
-        print(lines)
         # Unire tutti i byte in una sola stringa
         data_str = b''.join(lines).decode('utf-8')
         # Creiamo un oggetto StringIO
         data = io.StringIO(data_str)
         # Leggiamo i dati in un DataFrame
         df = pd.read_csv(data, sep='\t')
-        print(df)
+        #print(df)
+
+        print('Colonne nel DataFrame:', df.columns)  # Debugging
+        # Verifica le colonne presenti nel DataFrame
+        #qua interessante perche'se hai l'errore vai nel return ed esci dalla funzione madre...
+
+        #if x_column not in df.columns or y_column not in df.columns:
+        #    return JsonResponse({'error': f'Il file CSV deve contenere le colonne {x_column} e {y_column}'})
+
+        
+
+        # Esegui l'interpolazione
+        x = df[x_column]
+        print(x)
+        y = df[y_column]
+        f = interpolate.interp1d(x, y, kind='linear')  # Cambia 'linear' con il tipo di interpolazione desiderato
+        interpolated_x = x  # Puoi usare gli stessi valori x del CSV come punti interpolati
+        interpolated_y = f(interpolated_x)  # Esegui l'interpolazione
+
+        # Restituisci i risultati dell'interpolazione come JSON
+        result_data = {'x_values': interpolated_x.tolist(), 'y_values': interpolated_y.tolist()}
+        return JsonResponse({'result': result_data})
 
 
-        try:
-            # Verifica le colonne presenti nel DataFrame
-            if x_column not in df.columns or y_column not in df.columns:
-                return JsonResponse({'error': f'Il file CSV deve contenere le colonne {x_column} e {y_column}'})
-
-            print('Colonne nel DataFrame:', df.columns)  # Debugging
-
-            # Esegui l'interpolazione
-            x = df[x_column]
-            y = df[y_column]
-            f = interpolate.interp1d(x, y, kind='linear')  # Cambia 'linear' con il tipo di interpolazione desiderato
-            interpolated_x = x  # Puoi usare gli stessi valori x del CSV come punti interpolati
-            interpolated_y = f(interpolated_x)  # Esegui l'interpolazione
-
-            # Restituisci i risultati dell'interpolazione come JSON
-            result_data = {'x_values': interpolated_x.tolist(), 'y_values': interpolated_y.tolist()}
-            return JsonResponse({'result': result_data})
-
-        except Exception as e:
-            return JsonResponse({'error': 'Errore durante l\'analisi dei dati: {}'.format(str(e))})
 
     else:
         return JsonResponse({'error': 'Metodo non consentito'})
