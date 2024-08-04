90aea75c6663f0a8aa27b4a540e592026b6b145b
Gianvito Calabrese <gcalabrese296@gmail.com>
2024-08-04 20:28:03 +0200

    finally dataframe can be loaded

--- curve_analyzer/curve_app/static/js/main.js
+++ curve_analyzer/curve_app/static/js/main.js
@@ -4,11 +4,16 @@ new Vue({
     fileInput: null,
     analysisResult: null,
     scatterChart: null,
+    columnOptions: [], // Inizializza come array vuoto
+    selectedXColumn: '', // Inizializza come stringa vuota
+    selectedYColumn: '', // Inizializza come stringa vuota
   },
   methods: {
     submitData() {
       const formData = new FormData();
       formData.append('data', this.fileInput);
+      formData.append('x_column', this.selectedXColumn);
+      formData.append('y_column', this.selectedYColumn);
 
       fetch('/analyze_data/', {
         method: 'POST',
@@ -19,6 +24,9 @@ new Vue({
         console.log('Dati ricevuti dalla chiamata API:', data);
         if ('result' in data) {
           this.analysisResult = data.result;
+          if (this.scatterChart) {
+            this.scatterChart.destroy(); // Distruggi il grafico precedente se esiste
+          }
           this.scatterChart = this.plotScatter(data); // Crea e traccia il grafico con i dati ricevuti
         } else {
           console.error('Errore durante l\'analisi dei dati:', data.error);
@@ -30,6 +38,23 @@ new Vue({
     },
     fileChanged(event) {
       this.fileInput = event.target.files[0];
+      
+      // Estrai le colonne dal file CSV e aggiorna i menu a discesa
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target.result;
+        const lines = text.split('\n');
+        if (lines.length > 0) {
+          const headers = lines[0].split('\t');
+          console.log('Colonne estratte:', headers); //debugging
+          this.columnOptions = headers; // Popola le opzioni delle colonne
+          if (this.columnOptions.length > 0) {
+            this.selectedXColumn = this.columnOptions[0]; // Seleziona la prima colonna per x di default
+            this.selectedYColumn = this.columnOptions[1] || this.columnOptions[0]; // Seleziona la seconda colonna per y di default
+          }
+        }
+      };
+      reader.readAsText(this.fileInput);
     },
     plotScatter(data) {
       // Assicurati che i dati siano nel formato corretto per il tracciamento dello scatter plot utilizzando Chart.js
@@ -44,10 +69,7 @@ new Vue({
         data: {
           datasets: [{
             label: 'Scatter Plot',
-            data: {
-              x: data.result.x_values,
-              y: data.result.y_values,
-            },
+            data: data.result.x_values.map((x, index) => ({ x, y: data.result.y_values[index] })),
             backgroundColor: 'rgba(75, 192, 192, 1)',
           }],
         },
--- curve_analyzer/curve_app/views.py
+++ curve_analyzer/curve_app/views.py
@@ -1,37 +1,49 @@
-# views.py
 from django.shortcuts import render
 from django.http import JsonResponse
 import pandas as pd
 from scipy import interpolate
+import numpy as np
+import io
 
 def index(request):
     return render(request, 'index.html')
 
 def analyze_data(request):
     if request.method == 'POST':
-        # Controlla se il file è stato correttamente ricevuto
         if 'data' not in request.FILES:
             return JsonResponse({'error': 'File non ricevuto'})
-
+        
         file = request.FILES['data']
+        x_column = request.POST.get('x_column', 'x')  # Ottieni il nome della colonna x dal POST
+        y_column = request.POST.get('y_column', 'y')  # Ottieni il nome della colonna y dal POST
+
+        print(f'Colonna x selezionata: {x_column}')  # Debugging
+        print(f'Colonna y selezionata: {y_column}')  # Debugging
+
+        lines = file.readlines()
+        print(lines)
+        # Unire tutti i byte in una sola stringa
+        data_str = b''.join(lines).decode('utf-8')
+        # Creiamo un oggetto StringIO
+        data = io.StringIO(data_str)
+        # Leggiamo i dati in un DataFrame
+        df = pd.read_csv(data, sep='\t')
+        print(df)
+
 
         try:
-            # Leggi il file CSV con pandas
-            df = pd.read_csv(file)
-
             # Verifica le colonne presenti nel DataFrame
-            if 'x' not in df.columns or 'y' not in df.columns:
-                return JsonResponse({'error': 'Il file CSV deve contenere colonne x e y'})
+            if x_column not in df.columns or y_column not in df.columns:
+                return JsonResponse({'error': f'Il file CSV deve contenere le colonne {x_column} e {y_column}'})
+
+            print('Colonne nel DataFrame:', df.columns)  # Debugging
 
             # Esegui l'interpolazione
-            x = df['x']
-            y = df['y']
+            x = df[x_column]
+            y = df[y_column]
             f = interpolate.interp1d(x, y, kind='linear')  # Cambia 'linear' con il tipo di interpolazione desiderato
             interpolated_x = x  # Puoi usare gli stessi valori x del CSV come punti interpolati
             interpolated_y = f(interpolated_x)  # Esegui l'interpolazione
-
-            # Puoi fare ulteriori elaborazioni sui dati interpolati se necessario
-            # Ad esempio, puoi calcolare medie, massimi, minimi, ecc.
 
             # Restituisci i risultati dell'interpolazione come JSON
             result_data = {'x_values': interpolated_x.tolist(), 'y_values': interpolated_y.tolist()}
